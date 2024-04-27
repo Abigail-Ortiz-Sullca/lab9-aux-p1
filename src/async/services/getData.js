5a53eb531ef7c6af4b56e7996a24a71b6579c37f
OrtizAbigail <amoscma469@gmail.com>
2024-04-26 22:17:13 -0400

    adding files

--- src/async/services/getData.js
+++ src/async/services/getData.js
@@ -1,6 +1,7 @@
 ( async () => {
+  const id = Math.floor(Math.random() * 100) + 1;
 
-  const response = await fetch('https://dummyjson.com/products/1');
+  const response = await fetch(`https://dummyjson.com/products/${id}`);
   
   const data = await response.json();
 
